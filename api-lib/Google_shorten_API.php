cb00006e2c8c9f002e1d9e0c55f55e62a5724f84
MuhBayu <bnugraha00@hotmail.com>
2017-04-16 17:38:07 +0700

    fixed

--- api-lib/Google_shorten_API.php
+++ api-lib/Google_shorten_API.php
@@ -12,7 +12,7 @@ class Google_Shorten
 	protected static $_URI_SERVER;
 	protected static $_SSL_VERIFY;
 	protected static $_projection;
-	function __construct()
+	public function __construct()
 	{
 		$this->_projection = 'FULL'; // DEFAULT-nya FULL
 		$this->_SSL_VERIFY = FALSE; // DEFAULT-nya FALSE
@@ -28,13 +28,13 @@ class Google_Shorten
 	}
 	public function getProjection() {
 		$projection = [
-						'FULL' => 'returns the creation timestamp and all available analytics',
-						'ANALYTICS_CLICKS' => 'returns only click counts',
-						'ANALYTICS_TOP_STRINGS' => 'returns only top string counts (e.g. referrers, countries, etc'
-					];
+				'FULL' => 'returns the creation timestamp and all available analytics',
+				'ANALYTICS_CLICKS' => 'returns only click counts',
+				'ANALYTICS_TOP_STRINGS' => 'returns only top string counts (e.g. referrers, countries, etc'
+			];
 		return $projection;
 	}
-	function shortURL($_URL) {
+	public function shortURL($_URL) {
 		$postData = ['longUrl' => $_URL];
 		$jsonData = json_encode($postData);
 		$ch = curl_init();
@@ -45,16 +45,16 @@ class Google_Shorten
 		curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-type:application/json'));
 		curl_setopt($ch, CURLOPT_POST, 1);
 		curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonData);
-		$response 	 = curl_exec($ch);
-		$status_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-		$json 		 = json_decode($response);
+		$response 	= curl_exec($ch);
+		$status_code 	= curl_getinfo($ch, CURLINFO_HTTP_CODE);
+		$json 		= json_decode($response);
 		curl_close($ch);
 		if($status_code == 200) {
 			return json_encode($json, JSON_PRETTY_PRINT);	
 		}
 	}
-	function expandURL($shortURL) {
-		$params = 'key='.$this->_APIKEY.'&shortUrl='.$shortURL.'&projection='.$this->_projection;
+	public function expandURL($_URL) {
+		$params = 'key='.$this->_APIKEY.'&shortUrl='.$_URL.'&projection='.$this->_projection;
 		$ch = curl_init();
 		curl_setopt($ch, CURLOPT_URL, 'https://www.googleapis.com/urlshortener/v1/url?'.$params);
 		curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
@@ -62,11 +62,11 @@ class Google_Shorten
 		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, $this->_SSL_VERIFY);
 		curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-type:application/json'));
 		curl_setopt($ch, CURLOPT_HTTPGET, 1);
-		$response 	 = curl_exec($ch);
-		$status_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-		$json 		 = json_decode($response);
+		$response 	= curl_exec($ch);
+		$status_code 	= curl_getinfo($ch, CURLINFO_HTTP_CODE);
+		$json 		= json_decode($response);
 		curl_close($ch);
-			return json_encode($json, JSON_PRETTY_PRINT);	
+		return json_encode($json, JSON_PRETTY_PRINT);	
 		
 	}
 }
