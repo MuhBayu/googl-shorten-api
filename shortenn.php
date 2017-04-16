db9670a089b047c31e63fc5b3b2853cdc399cf6f
MuhBayu <bnugraha00@hotmail.com>
2017-04-16 17:27:49 +0700

    Rename shorten.php to shortenn.php

---
+++ shortenn.php
@@ -0,0 +1,28 @@
+<?php
+header('Content-type:application/json');
+header('Access-Control-Allow-Origin: *'); 	
+require_once('api-lib/Google_shorten_API.php');
+
+/**
+*  FUNCTION :
+* for SHORT URL -----> shortURL()
+* for EXPAND URL ----> expandURL()
+*/
+
+$long_Url = 'http://bayuu.net';
+
+$googl = new Google_Shorten();
+$googl->setApiKey('YOUR_API_KEY');  // Masukan API_KEY kamu disini
+$googl->setSSL(false);              // SSL_VERIFY
+
+$response_output = $googl->shortURL($long_Url);
+$response_parse = json_decode($response_output);
+
+if(isset($response_parse->{'kind'})) {
+	echo $response_output;
+} else {
+	echo "gagal, ada error...";
+}
+
+
+?>
